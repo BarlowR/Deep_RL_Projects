e90ac590b88945753e3b11524a82aa8d3f5124b5
BarlowR <robbarlw@gmail.com>
2020-12-20 21:04:36 -0500

    made process count cpu dependent

--- src/policy_gradient/policy_gradient.py
+++ src/policy_gradient/policy_gradient.py
@@ -200,7 +200,8 @@ class policy_estimator_network():
 
             batches = []
 
-            with mp.get_context("spawn").Pool(processes=4) as p:
+
+            with mp.get_context("spawn").Pool(processes=multiprocessing.cpu_count()) as p:
                 batches = p.map(self.run_episode, self.environments)
             #batches = [self.run_episode(self.environments[i]) for i in range (4)]
             return batches
